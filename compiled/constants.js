5ea695fe29a66557b51d5e157f1f68c1d5180efb
soruh <mail@soruh.de>
2021-10-04 18:58:31 +0200

    stop distributing compiled versions

--- compiled/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MAX_BAUDOT_DATA_SIZE = void 0;
-const MAX_BAUDOT_DATA_SIZE = 16;
-exports.MAX_BAUDOT_DATA_SIZE = MAX_BAUDOT_DATA_SIZE;
